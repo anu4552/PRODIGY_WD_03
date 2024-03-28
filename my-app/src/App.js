89becbe87d0218b9fcfe7fa2a8e587dbfc3d82b4
anu kumari <anukumari95634@gmail.com>
2024-03-28 22:55:52 +0530

    My project

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
+
+
+function App() {
+  return (
+    
+      <><TicTacToe/></>
+    
+  );
+}
+
+export default App;
